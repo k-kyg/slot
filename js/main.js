ec8703dc6499f1d1fc92ba06928710b4d904136a
k-kyg <kygkd38@gmail.com>
2019-10-05 23:55:44 +0900

    coinの機能つけた！

--- js/main.js
+++ js/main.js
@@ -1,11 +1,18 @@
 const first = document.getElementById("num1"),
 	second = document.getElementById("num2"),
-	third = document.getElementById("num3");
-let status = [],
-	playing = false;
+	third = document.getElementById("num3"),
+	cointag = document.getElementById("coin"),
+	rat = document.getElementsByClassName("rating")[0];
 function* slotnum() {
-	yield* [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+	yield* [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
 }
+let status = [],
+	playing = false,
+	coin = 1000,
+	fir = slotnum(),
+	snd = slotnum(),
+	tir = slotnum(),
+	bet;
 window.onkeydown = () => {
 	switch (event.keyCode) {
 		case 13: start(); break;
@@ -17,10 +24,8 @@ window.onkeydown = () => {
 }
 const start = () => {
 	if (playing) return void (0);
-	let fir = slotnum(),
-		snd = slotnum(),
-		tir = slotnum();
 	playing = true;
+	bet = parseInt(document.getElementById("bet").value),
 	window.f = setInterval(() => {
 		let num = fir.next();
 		if (num.done) {
@@ -56,7 +61,43 @@ const stop = x => {
 		status = Array.from(new Set(status));
 		if (status.length === 3) {
 			status = [];
+			check(...[first.textContent, second.textContent, third.textContent].map(x => parseInt(x)));
 			playing = false;
 		}
+	}
+}
+const changecoin = x => void(cointag.textContent = coin += bet * x);
+const check = (x, y, z) => {
+	const sortedarr = [x, y, z].sort((a, b) => a - b);
+	const arr = [x, y, z];
+	const subarr = [Math.abs(x - y), Math.abs(y - z)].sort((a, b) => a - b);
+	if (x === y && y === z) {
+		switch (x) {
+			case 7:
+			case 8:
+				rat.textContent = "Perfect!";
+				changecoin(10);
+				break;
+			default:
+				rat.textContent = "Amazing!";
+				changecoin(9);
+		}
+	} else if ((x > y && y > z) || (x < y && y < z)) {
+		if ((x > z && x === y * (y / z)) || (x < z && z === y * (y / x))) {
+			rat.textContent = "Fantastic!";
+			changecoin(8);
+		} else if (subarr[0] === subarr[1]) {
+			rat.textContent = "Excelent!";
+			changecoin(6);
+		} else if (Math.abs(subarr[0] - subarr[1]) === 1) {
+			rat.textContent = "Good!";
+			changecoin(4);
+		}
+	} else if (x === z) {
+		rat.textContent = "Great!";
+		changecoin(7);
+	} else if (x === y || y === z) {
+		rat.textContent = "OK";
+		changecoin(3);
 	}
 }
